f3768fdb1e52c638967dc6f0f74184a48eb840d7
zcphoenix2016 <13564240769@139.com>
2016-11-17 13:33:03 +0800

    Refactor the implementation

--- Include/MorseDec.hpp
+++ Include/MorseDec.hpp
@@ -5,5 +5,14 @@
 class MorseDec
 {
 public:
-  static std::string decodeBits(const std::string&);
+    static std::string decodeBits(const std::string&);
+
+private:
+    static std::string processContinuousOnes(auto p_iterOne, auto p_iterZero);
+    static std::string processContinuousZeros(auto p_iterOne, auto p_iterZero);
+    
+    static unsigned int calcRate()
+    {
+        return 1;
+    }
 };
--- Sources/MorseDec.cpp
+++ Sources/MorseDec.cpp
@@ -2,35 +2,48 @@
 #include <algorithm>
 
 
+std::string MorseDec::processContinuousOnes(auto p_iterOne, auto p_iterZero)
+{
+    if((p_iterZero - p_iterOne)/calcRate() == 3)
+    {
+        return "-";
+    }
+    if((p_iterZero - p_iterOne)/calcRate() == 1)
+    {
+        return ".";
+    }
+    
+    return "";
+}
+
+std::string MorseDec::processContinuousZeros(auto p_iterOne, auto p_iterZero)
+{
+    if((p_iterOne - p_iterZero)/calcRate() == 3)
+    {
+        return " ";
+    }
+    if((p_iterOne - p_iterZero)/calcRate() == 7)
+    {
+        return "   ";
+    }
+    
+    return "";
+}
+
 std::string MorseDec::decodeBits(const std::string& p_str)
 {
     std::string l_result;
     int l_rate = 1;
     auto l_point2One = std::find(p_str.begin(), p_str.end(), '1');
     auto l_point2Zero = l_point2One;
-     
+
     while(l_point2One !=  p_str.end())
     {
         l_point2Zero = std::find(l_point2One, p_str.end(), '0');
-        if((l_point2Zero - l_point2One)/l_rate == 3)
-        {
-            l_result += "-";
-        }
-        if((l_point2Zero - l_point2One)/l_rate == 1)
-        {
-            l_result += ".";
-        }
+        l_result += processContinuousOnes(l_point2One, l_point2Zero);
 
         l_point2One = std::find(l_point2Zero, p_str.end(), '1');
-        
-        if((l_point2One - l_point2Zero)/l_rate == 3)
-        {
-            l_result += " ";
-        }
-        if((l_point2One - l_point2Zero)/l_rate == 7)
-        {
-            l_result += "   ";
-        }
+        l_result += processContinuousZeros(l_point2One, l_point2Zero);       
     }
     return l_result;
 }
