247d1cb5bc4d0e6c70bd141520e7b52befff1a2b
zcphoenix2016 <13564240769@139.com>
2016-11-17 11:49:14 +0800

    test case that return a dash

--- Sources/MorseDec.cpp
+++ Sources/MorseDec.cpp
@@ -1,15 +1,42 @@
 #include "../Include/MorseDec.hpp"
+#include <algorithm>
+
 
 std::string MorseDec::decodeBits(const std::string& p_str)
 {
     std::string l_result;
-    for(int i = 0;i < p_str.size();i++)
+    
+    
+    
+/*    for(int i = 0;i < p_str.size();i++)
     {
+       
        if('1' == p_str[i])
        {
-           l_result+='.';
+           l_result+= p_str[i];
        }
+       
+       
            
+    }*/
+    int l_rate = 1;
+    auto l_point2One = p_str.begin();
+    l_point2One = std::find(p_str.begin(), p_str.end(), '1');
+    auto l_point2Zero = l_point2One;
+     
+    while(l_point2One !=  p_str.end())
+    {
+        l_point2Zero = std::find(l_point2One, p_str.end(), '0');
+        if((l_point2Zero - l_point2One)/l_rate == 3)
+        {
+            l_result += "-";
+        }
+        if((l_point2Zero - l_point2One)/l_rate == 1)
+        {
+            l_result += ".";
+        }
+
+        l_point2One = std::find(l_point2Zero, p_str.end(), '1');
     }
     return l_result;
 }
--- TestCases/MorseDecTestSuite.cpp
+++ TestCases/MorseDecTestSuite.cpp
@@ -2,16 +2,23 @@
 #include <string>
 #include "../Include/MorseDec.hpp"
 
-TEST(MorseDecTestSuite, TheOutputShouldBeDotWithOneBit)
+TEST(MorseDecTestSuite, TheOutputShouldBeOneDot)
 {
     std::string l_str("1");
     
     ASSERT_EQ(".", MorseDec::decodeBits(l_str));
 }
 
-TEST(MorseDecTestSuite, TheOutputShouldBeTwoDotsWithTreeBits)
+TEST(MorseDecTestSuite, TheOutputShouldBeTwoDots)
 {
     std::string l_str("101");
     
     ASSERT_EQ("..", MorseDec::decodeBits(l_str));
+}
+
+TEST(MorseDecTestSuite, TheOutputShouldBeDashAndDot)
+{
+    std::string l_str("11101");
+    
+    ASSERT_EQ("-.", MorseDec::decodeBits(l_str));
 }
